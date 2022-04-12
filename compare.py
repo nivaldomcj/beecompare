fcd8f25feb196ef64241919c09f4858e26e6f6fa
Nivaldo Carvalho <nivaldo_j@outlook.com>
2022-04-12 10:13:43 -0300

    ¯\ _(ツ )_/¯

--- compare.py
+++ compare.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 import csv
+import time
 
 
 def get_next_url(base_url, soup):
@@ -16,10 +17,12 @@ def is_problem(item):
 def solved_from(user_code):
     solved_problems = set()
 
-    base_url = "https://www.urionlinejudge.com.br"
+    base_url = "https://beecrowd.com.br"
     next_url = "{}/judge/pt/profile/{}".format(base_url, user_code)
 
     while next_url is not None:
+        print("[DEBUG] (User {}) Fetching URL: {}".format(user_code, next_url))
+
         html = requests.get(next_url).text
         soup = BeautifulSoup(html, "html.parser")
 
@@ -27,13 +30,20 @@ def solved_from(user_code):
 
         for item in table:
             if is_problem(item):
+                print("[DEBUG] (User {}) Found a problem solved. Trying to parse it...".format(user_code))
                 problem_code = item.find("td", class_="id").a.text
                 problem_name = item.find("td", class_="wide").a.text
                 problem_link = "{}{}".format(base_url, item.find("td", class_="wide").a["href"])
 
+                print("[DEBUG] (User {}) Adding problem {}".format(user_code, problem_code))
                 solved_problems.add((problem_code, problem_name, problem_link))
 
         next_url = get_next_url(base_url, soup)
+
+        # avoid being banned because of scraping
+        print("[DEBUG] I will sleep for a while, so they cannot catch me! =]")
+        time.sleep(2)
+
 
     return solved_problems
 
@@ -51,15 +61,19 @@ def username(user_code):
 
 
 def save(user_names, problems):
+    print("[DEBUG] Saving problems from ({})".format(user_names))
+
     with open("UOJ - {} vs {}.csv".format(user_names[0], user_names[1]), "w", newline="") as file:
         writer = csv.writer(file, delimiter=',', quotechar='"')
 
         _ = writer.writerow(("Problem Code", "Problem Name", "Problem Link"))
         _ = [writer.writerow((item[0], item[1], item[2])) for item in problems]
+    
+    print("[DEBUG] Saved files! My job here is done.")
 
 
 def main(user_codes):
-    print("working...")
+    print("[DEBUG] Ok, here we go. You have set {} and {} User Codes.".format(user_codes[0], user_codes[1]))
 
     problems = compare(user_codes)
     usernames = [username(user_codes[0]), username(user_codes[1])]
@@ -67,7 +81,7 @@ def main(user_codes):
     save([usernames[0], usernames[1]], problems[1])
     save([usernames[1], usernames[0]], problems[0])
 
-    print("finished!")
+    print("[DEBUG] ~~Flies away")
 
 
 if __name__ == "__main__":
